68741f6d46125210744d217e3f2e61edaf9ccb07
Geoff Ostman <81042162+geoff-ostman@users.noreply.github.com>
2021-05-21 16:07:20 -0500

    local storage, event handler

--- timeblocks.js
+++ timeblocks.js
@@ -29,5 +29,26 @@ function setColor(element, color) {
   element.style.backgroundColor = color;
 }
 
-// Local Storage
-document.getElementById("my-btn").addEventListener('click', console.log ("hello"));
+// Save To Local Storage
+var saveTask = function() {
+    localStorage.setItem("task", JSON.stringify(tasks));
+  };
+  
+  var loadTask = function() {
+    var savedTasks = localStorage.getItem("task");
+    // if there are no tasks, set tasks to an empty array and return out of the function
+    if (!savedTasks) {
+      return false;
+    }
+    console.log("Saved tasks found!")};
+
+// Create New Task
+formEl.addEventListener("task", taskFormHandler);
+
+// for edit and delete buttons
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+// for changing the status
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
